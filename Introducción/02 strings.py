8f5f17947a104b5dcbf56efc5ac11e9324601bcd
Juan Jose <carrerotalora15@gmail.com>
2024-07-09 13:16:12 -0500

    cuarto commit clase 4

--- Introducción/02 strings.py
+++ Introducción/02 strings.py
@@ -1,2 +1,6 @@
-print(150 + 125)
-print("150 + 125")
+#print(150 + 125)
+#print("150 + 125")
+#print("Juan" + "Jose")
+#print("Juan " + "Jose")
+print("Juan" + " Jose")
+print("Juan" + " " + "Jose")
